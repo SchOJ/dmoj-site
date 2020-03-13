9133d16d32424292e863cf223c9efa89d3251bb5
Outvi V <19144373+outloudvi@users.noreply.github.com>
2020-03-13 20:52:52 +0800

    fix: start.sh typo and preserve mounted /site

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -e /install_done ]; then
-    if [ -e /osite/site ]; then
+    if [ ! -e /site/manage.py ]; then
     cp -r --preserve=all /osite/site /
     mycli -h db -u root -p dmoj --execute="CREATE DATABASE dmoj DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;"
     python3 manage.py migrate
@@ -8,7 +8,8 @@ if [ ! -e /install_done ]; then
     python3 manage.py loaddata language_small
     # The next line is optional
     #python manage.py loaddata demo
-touch /install_done
+    fi
+    touch /install_done
 fi
 service nginx start
 service supervisor start
